0ead903c0c139cdae4beaa42c4506788fa1df35b
Nahia Akter <nhaktr@gmail.com>
2020-08-06 16:27:20 -0400

    App.js updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,28 +1,64 @@
 /* Author: Nahia Akter
    Date  : 03/07/2020
-   Description: JavaScript file for html page
-*/
+   Description: JavaScript file for html page*/
+   
 (function(){
 
-    // named function
-        
-    function Start()
+    // named function     
+    function loadContent()
     {
-        console.log('%cApp Started...', "color:white; font-size: 24px;");   
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
 
-       let title = document.title;
+        // step 2 - sets the type of the message to be read as JSON
+        XHR.responseType = 'json';
 
-       title = title.toLowerCase();
+        // step 3 - configures the message
+        XHR.open("GET", "./Scripts/paragraphs.json");
 
-       console.log(`The title of the page is ${title}`);
+        // step 4 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                //stores the JSON content of the AboutMe object
+                let Contents = XHR.response.paragraphs;
+
+                //calls the appropriate function to load contents based on the title of the page
+                switch (document.title) {
+                    case "About me":
+                        AboutMeContent(Contents[0]);
+                        break;
+                    case "Projects":
+                        ProjectsContent(Contents[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+                
+        });
+
+        //Again, method call is placed here to counter the async nature of JavaScript
+        loadFooter();
 
     }
-    
-    window.addEventListener("load", Start);
+    function aboutMeText(aboutMe)
+    {
+        //sets values from the read JSON array's first object appropriately  
+        document.getElementsByTagName("h2")[0].innerText = aboutMe.id;
+        document.getElementsByTagName("h2")[1].innerText = aboutMe.missionid;
+        document.getElementsByTagName("p")[0].innerText = aboutMe.introduction;
+        document.getElementsByTagName("blockquote")[0].innerText = aboutMe.missionStatement;
+        document.getElementsByTagName("h2")[2].innerText = aboutMe.hobbiesid;
+        document.getElementsByClassName("ul")[0].innerHTML = aboutMe.hobbies;
 
-    let title = document.querySelector("title").innerText;
+    }
 
-    // function for home page
+
+
+    /* function for home page
      function aboutMeText()
     {
         let pString =
@@ -36,7 +72,7 @@
         document.getElementsByTagName("p")[0].innerText = pString;
         document.getElementsByTagName("blockquote")[0].innerText = mission;
         document.getElementsByTagName("h2")[2].innerText = "Favourite Things To do:";
-    }
+    }*/
     // Function for project page
     function project()
     {
@@ -53,7 +89,7 @@
         "and manipulation tool, built on top of the Python programming language.this semester I'm going to learn more about Python."
     }
 
-    //switch title and load the page
+    /*switch title and load the page
     switch (title) {
         case "About me":
             window.addEventListener("load", aboutMeText);
@@ -62,7 +98,76 @@
             window.addEventListener("load", project);
     
         default:
-            break;
-    } 
+            break;}*/
+    
+    function loadHeader()
+    {
+        console.info("Header is loading...");
 
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - configures the message
+        XHR.open("GET", "./views/partials/header.html");
+
+        // step 3 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                let header = document.getElementsByTagName("header")[0];
+
+                let headerData = XHR.responseText;
+
+                header.innerHTML = headerData;
+                
+                setContent();
+            }
+        });
+    }
+    function loadFooter()
+    {
+        console.info("Footer is loading...");
+
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - configures the message
+        XHR.open("GET", "./views/partials/footer.html");
+
+        // step 3 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                let footer = document.getElementsByTagName("footer")[0];
+                
+                let footerData = XHR.responseText;
+
+                footer.innerHTML = footerData;
+            }
+        });
+    }
+
+    function Start()
+    {
+        console.log('%cApp Started...', "color:white; font-size: 24px;");   
+
+       let title = document.title;
+
+       title = title.toLowerCase();
+
+       console.log(`The title of the page is ${title}`);
+
+       loadHeader();
+    }
+    
+    window.addEventListener("load", Start);
+
+    //let title = document.querySelector("title").innerText;
+
+
+   
 })();
